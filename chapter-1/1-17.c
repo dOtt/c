d757063f783b93b322271c6a91c4403260f005d6
Vincent Gu <0x6c34@gmail.com>
2012-03-29 02:14:29 +0800

    modify exercise 1-17

--- chapter-1/1-17.c
+++ chapter-1/1-17.c
@@ -4,7 +4,7 @@
  * Write a program to print all input lines that are longer than 80 characters.
  */
 #include <stdio.h>
-#define LINE_LIMIT              /* maximum input line length */
+#define LINE_LIMIT 5            /* maximum input line length */
 
 int my_readline();
 
@@ -12,57 +12,20 @@ int my_readline();
 main()
 {
   char line[LINE_LIMIT];
+  int i, j;
+  char c;
 
-  while ((len = my_readline()) > 0) {
+  for (i=0; i<LINE_LIMIT && (c=getchar()) != EOF && c != '\n'; i++) {
+    line[i] = c;
   }
-}
-
-int my_readline(char line[]) {
-  for (int i=0; i < LINE_LIMIT && (c=getchar())!=EOF && c!='\n'; i++)
-    
-  if (i < LINE_LIMIT) {
-    return i;
-  }
-  
-}
-    
-
-  int len;                      /*current line length */
-  int max;                      /*maximum length seen so far */
-  char line[MAXLINE];           /* current input line */
-  char longest[MAXLINE];        /* longest line saved here */
-
-  max = 0;
-  while ((len = my_getline(line, MAXLINE)) > 0)
-    if (len > max) {
-      max = len;
-      copy(longest, line);
+  if (i == LINE_LIMIT) {
+    for (j=0; j <LINE_LIMIT; j++) {
+      putchar(line[j]);
     }
-  if (max > 0)                  /* there was a line */
-    printf("longest line[%d chars]: %s", max, longest);
-  return 0;
-}
-
-/* my_getline: read a line into s, return length */
-int my_getline(char s[], int lim)
-{
-  int c, i;
-  for (i=0; (c=getchar())!=EOF && c!='\n'; ++i) {
-      s[i] = c;
+    while ((c=getchar()) != EOF && c != '\n') {
+      putchar(c);
+    }
+    if (c == '\n')
+      putchar(c);
   }
-  if (i < lim-1 && c == '\n') {
-    s[i] = c;
-    ++i;
-  }
-  s[lim-1] = '\0';
-  return i;
-}
-
-/* copy: copy 'from' into 'to'; assume to is big enough */
-void copy(char to[], char from[])
-{
-  int i;
-  i = 0;
-  while ((to[i] = from[i]) != '\0')
-    ++i;
 }
