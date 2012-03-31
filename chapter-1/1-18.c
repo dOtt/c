3075b53159d99a7d20d71a3757846686a829244a
Vincent Gu <0x6c34@gmail.com>
2012-03-31 13:46:35 +0800

    finish exercise 1-18

--- chapter-1/1-18.c
+++ chapter-1/1-18.c
@@ -3,18 +3,38 @@
  * line of input, and to delete entirely blank lines.
  */
 #include <stdio.h>
-#define CHAR_BUF 100            /* backtrack character buffer size */
+#define CHAR_BUF_SIZE 100       /* backtrack character buffer size */
 
 main() {
-  char c, char buff[CHAR_BUF];
-  int i, j;
-  int bp=-1, trail;
+  char c='\0', buff[CHAR_BUF_SIZE];
+  int i, j, k, trail;
 
-  while ((c=getchar()) != EOF) {
-    for (i=0; i != '\n'; i++) {
-      if (trail == 1) {
-        buff[j++] = c;
+  i = j = k = trail = 0;
+  while (c != EOF) {
+    for (i=0; (c=getchar()) != '\n' && c != EOF; i++) {
+      if (c == ' ' || c == '\t') { /* special characters */
+        if (trail != 1) {          /* enter trail-mode if not in already */
+          trail = 1;
+        }
+        if (j < CHAR_BUF_SIZE) { /* prevent overflow */
+          buff[j++] = c;
+        }
+      } else {                  /* normal characters */
+        if (trail == 1) {       /* exit trail-mode, also print all chars */
+          for (k=0; k < j; k++) {
+            putchar(buff[k]);
+          }
+          trail = j = 0;
+        }
+        putchar(c);
       }
-      if ((c == ' ' || c == '\t') && bp == -1) {
-        bp = i;
-      }
+    }
+    /* line ends */
+    if (c == '\n' && i != j) {  /* skip empty lines */
+      putchar(c);
+    }
+    /* reset trail buffer */
+    trail = j = 0;
+  }
+  /* input ends */
+}
